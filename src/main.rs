1504e07d4a22cca3c4c7dee0d934cc16f60b6a08
Evan <evantri.edt@gmail.com>
2026-04-05 02:55:59 -0500

    added walking directory, computing hash, check if dir exists

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use walkdir::WalkDir;
 use blake3::Hasher;
 
 fn main() {
+
+    // Collect arguments
     let args: Vec<String> = env::args().collect();
 
     if let Some(pos) = args.iter().position(|arg| arg == "--") {
         let cmd_args = &args[pos + 1..];
-        println!("{:?}", cmd_args);
 
+    //Walk directory
         let entries: Vec<_> = WalkDir::new(".")
         .into_iter()
         .filter_map(|e| e.ok())
@@ -16,29 +18,42 @@ fn main() {
         .filter(|e| !should_skip(e.path()))
         .collect();
 
+    //Compute hash
         let mut hasher = Hasher::new();
         for entry in &entries {
             let bytes = std::fs::read(entry.path()).unwrap_or_default();
             hasher.update(&bytes);
         }
+
         let hash_string = hasher.finalize().to_hex().to_string();
+
+        //Check if dir exists in cache
+        if let Some(cache_dir) = dirs::cache_dir() {
+            let blob_path = cache_dir
+                .join("cache-tool")
+                .join("blobs")
+                .join(&hash_string);
+
+            if std::fs::metadata(&blob_path).is_ok() {
+
+            } else {
+
+            }
+            
+        }
         
         let status = std::process::Command::new(&cmd_args[0])
             .args(&cmd_args[1..])
             .status()
             .expect("Failed to run command");
+
         std::process::exit(status.code().unwrap_or(1));
+
     } else {
         eprintln!("Missing arguments");
         std::process::exit(1);
     }
-
-    if let Some(cache_dir) = dirs::cache_dir() {
-        let blob_path = cache_dir
-            .join("cache-tool")
-            .join("blobs")
-            .join(&hash_string);
-    }
+  
 }
 
 fn should_skip(path: &std::path::Path) -> bool {
